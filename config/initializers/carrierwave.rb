058c9252ee2f8a677900c7d83a595658ca795126
Shobhab Shrestha <shobhabstha@gmail.com>
2014-01-26 18:44:15 +0545

    key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_credentials = {
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV["AWS_ACCESS_KEY_ID"],
-      :aws_secret_access_key => ENV["AWS_ACCESS_KEY"]
+      :aws_access_key_id     => 'AKIAIAGTVHE3GGLUYXQQ',
+      :aws_secret_access_key => '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'
     }
-    config.fog_directory    = ENV["AWS_BUCKET"]
+    config.fog_directory    = "Demophoto"
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
